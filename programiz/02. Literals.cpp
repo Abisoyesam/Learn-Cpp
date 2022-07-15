9667d92621535eaebf2c801ee407ad61b0ec3407
Abisoye Akinloye <abisoyesam@gmail.com>
2022-07-15 15:54:28 +0100

    variable and literal

--- programiz/02. Literals.cpp
+++ programiz/02. Literals.cpp
@@ -1,4 +1,32 @@
-int main(int argc, char *argv[])
-{
+/*
+	Literals are fixed value that are directly used in a program.
 	
+	The most commonly used literal in C++ are: integer, floating point, character.
+	
+	Char is enclosed in a single quotation. e.g 'a', '+', '1'
+	
+	Variables are used to store value so that they can later be used in the program.
+	
+	To declare variable, you need data type & variable name (identifier) end with semi colon. As in line 22.
+	
+	You can declare multiple variables as in line 24.
+	
+	 Use 'const' if you don't wanna override variable. 
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int age;
+
+int tax, salary = 4500;
+
+int main(){
+	age = 22;
+	
+	cout << age <<endl;
+	
+	return 0;
 }
---
+++ programiz/03. Variable Type & Size.cpp
@@ -0,0 +1,13 @@
+/*
+	Int can store integer usually in 4 bytes. (32 bits)	2^32 distinct integer.
+	
+	Both 'float' and 'double' stores number with decimal. Float is 4 bytes while double is 8 bytes.
+	
+	Always use 'double' for decimal no b'cos it has better precision. Float has 6-7 decimals while double has 15 decimals.
+	
+	Char is used to store single character. Always has 1 byte size and single quote.
+	
+	Bool: two value either true or false, and it 1 byte.
+	
+	String: to store sequence of characters (text). It's not built-in data type but it behave like one in its most usage.
+*/
